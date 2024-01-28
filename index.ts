beb496d9bf5df6965a471c1f5ea56e51bbcbb9f3
Chris Kong <xism360@gmail.com>
2024-01-28 03:04:43 -0600

    Added Google

--- index.ts
+++ index.ts
@@ -61,7 +61,7 @@ export const checkFileExists = async ({directory, fileName}: any) => {
 /////////////////////////////
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
+  apiKey: process.env.OPENAI_API_KEY!, // This is the default and can be omitted
 });
 
 const queryGPT = async ({ message }: {message: string}) => {
