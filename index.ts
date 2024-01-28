578da2172f9c345f958ee32e1394f8b62dda8c33
Chris Kong <xism360@gmail.com>
2024-01-28 06:00:19 -0600

    Major improvement

--- index.ts
+++ index.ts
@@ -44,6 +44,13 @@ const storage = new Storage(storageOptions);
 const bucketName = "pusheen";
 const bucket = storage.bucket(bucketName);
 
+const uploadByteToFile = async ({ data, title }: {data: any, title: string}) => {    
+  const file = bucket.file(title);
+  const contents = Buffer.from(data.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64');
+  
+  await file.save(contents).then(() => console.log(`Saved ${title}`));
+}
+
 export const checkFileExists = async ({directory, fileName}: any) => {
   const file = bucket.file(`${directory}/${fileName}`);
 
@@ -64,30 +71,124 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY!, // This is the default and can be omitted
 });
 
-const queryGPT = async ({ message }: {message: string}) => {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: message }],
-    model: 'gpt-3.5-turbo',
-  });
-};
+const extractSentences = (rawSentences: string) => {
+  const regex = /\(([^)]+)\)/g;
+
+  // Extract and return all matches
+  const sentences = [];
+  let match;
+  while ((match = regex.exec(rawSentences)) !== null) {
+      sentences.push(match[1]);
+  }
+
+  return sentences;
+}
 
 const getScript = async ({ prompt }: {prompt: string}) => {
-  const story = await queryGPT({
-    message: `You are a narrator for a children's educational story. The topic is to explain photosynthesis through a story that is both entertaining and understandable to a child. There is a main character named Pusheen in this story who has to discover the lesson about photosynthesis. This character is a cute cat who represents the child wanting to learn.
-    **Write the story as a series of scenes in a list numbered by their order. For example,
-    1. ... Scene 1 ...
-    2. ... Scene 2 ...
-    ...
-    DO NOT ADD TITLES TO EACH SCENE. You will be *penalized* for adding extra commentary or titles (e.g., describing as "Scene 1" for each scene or for forgetting the numbering).
-    **`
+  const messages: any = [
+    { role: 'user', content: `You are a narrator for a children's educational story. The topic is to explain ${prompt} through a story that is both entertaining and understandable to a child. There is a main character named Pusheen in this story who has to discover the lesson about ${prompt}. This character is a cute cat who represents the child wanting to learn.
+    **Write the story as a series of THREE scenes each enclosed within parantheses ( and ).
+    DO NOT ADD TITLES TO EACH SCENE. DO NOT GO OVER THREE SCENES. You will be *penalized* for adding extra commentary or titles (e.g., describing as "Scene 1" for each scene or for forgetting the numbering) and you will be heavily penalized for adding more than THREE scenes.
+    For example,
+    (Pusheen woke up)
+    (Pusheen did something)
+    (Then this happened)
+    **` }
+  ];
+
+  let response = await openai.chat.completions.create({
+    messages,
+    "model": "gpt-4-turbo-preview"
   });
+  if (!response) {
+    throw new Error("Error querying ChatGPT.");
+  }
+  const storyString = response.choices[0].message.content;
+
+  messages.push({
+    role: 'assistant',
+    content: storyString
+  }, {
+    role: 'user',
+    content: `Describe each scene  in vivid but CONCISE details. Do NOT use complete sentences - instead, describe what Pusheen is doing and where she is / her environment. 
+    You will be *penalized* heavily for being too wordy or deviating from this format. Please follow the same format as before with each scene's response enclosed within parantheses ( and ). For example,
+    You will be *penalized* for adding extra commentary or titles (e.g., describing as "Scene 1" for each scene or for forgetting the numbering) and you will be heavily penalized for adding more than THREE scenes.
+    For example,
+    (Pusheen sitting on a couch, bright morning, sun shining)
+    (Pusheen playing outside, sunflowers and tall grass, fluffy clouds)
+    (Pusheen in the school play, dancing with friends, show lights on)`
+  });
+  response = await openai.chat.completions.create({
+    messages,
+    "model": "gpt-4-turbo-preview"
+  });
+  if (!response) {
+    throw new Error("Error querying ChatGPT.");
+  }
+  const promptsString = response.choices[0].message.content;
+
+  const story = extractSentences(storyString!);
+  const prompts = extractSentences(promptsString!);
+
+  await getImages({ prompts });
+
+  return { story, prompts };
 }
 
 /////////////////////////////
 // SD API
 /////////////////////////////
 
+const getImage = async ({ prompt, sceneNumber }: {prompt: String, sceneNumber: Number}) => {
+  const params = {
+    "api": {
+      "method": "POST",
+      "endpoint": "/sdapi/v1/txt2img"
+    },
+    "payload": {
+      "override_settings": {
+        "sd_model_checkpoint": "base"
+      },
+      "override_settings_restore_afterwards": true,
+      "prompt": prompt + " (no humans) <lora:Pusheen:1>",
+      "negative_prompt": "embedding:negs\nsfwEM, too many legs, deformity",
+      "seed": -1,
+      "batch_size": 1,
+      "steps": 30,
+      "cfg_scale": 6,
+      "width": 512,
+      "height": 512,
+      "sampler_name": "DPM++ 2M Karras",
+      "sampler_index": "DPM++ 2M Karras",
+      "restore_faces": false
+    }
+  };
 
+  const response = await axios.post("https://api.runpod.ai/v2/yc2fkxtvuqg3rr/runsync", {
+    "input": params,
+  }, {
+    "headers": {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.RUNPOD_API_KEY}`
+    }
+  })
+  .then(async (res) => {
+    await uploadByteToFile({
+      data: res.data.output?.images[0],
+      title: `scene_${sceneNumber}.jpg`
+    });
+  }).catch(() => {
+    throw new Error(`Failed to process image ${sceneNumber}`);
+  });
+}
+
+const getImages = async ({ prompts }: { prompts: String[] }) => {
+  await Promise.all(
+    prompts.map((prompt, index) => {
+         return getImage({ prompt, sceneNumber: index });
+    })
+ );
+}
 
 /////////////////////////////
 // ELABS API
@@ -113,6 +214,13 @@ const jobWorker = new Worker("job",
   async (request: Job) => {
     const { jobId } = request.data;
     
+    const job = await prismadb.job.findUnique({ where: { id: jobId } });
+    if (!job) throw new Error("Job not found with id");
+    const storyPrompt = job.prompt;
+
+    const { story, prompts } = await getScript({ prompt: storyPrompt });
+    await getImages({ prompts });
+    // await getAudios({ prompts });
 
     return 'Job processed';
   }, 
@@ -202,3 +310,5 @@ app.listen(port, () => {
   console.log(`Public domain: ${process.env.RAILWAY_PUBLIC_DOMAIN}`);
   console.log(`Running on port ${port}...`);
 });
+
+getScript({ prompt: "photosynthesis" });
